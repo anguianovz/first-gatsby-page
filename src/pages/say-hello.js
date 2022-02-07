627954bb3563bcc2ad359b5cd04e30f444ce042c
Jesus <jesuzvz.a@protonmail.com>
2022-02-07 08:29:54 -0600

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/say-hello.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+// Rendering the <Greeting> component
+const SayHello = () => {
+  return (
+    <div>
+      <Greeting name="Megan" />
+      <Greeting name="Obinna" />
+      <Greeting name="Generosa" />
+    </div>
+  )
+}
